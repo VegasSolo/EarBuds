0423df686099a72cbc3a7132ced5b3eb2f30ed6d
ezimkin <nrmize@email.uark.edu>
2016-03-04 19:50:23 +0000

    added serverHandler and node_modules package for dispatcher

---
+++ Website/serverHandler.js
@@ -0,0 +1,34 @@
+var http = require('http');
+const PORT = process.env.PORT;
+const HOST = process.env.IP;
+var server = http.createServer(handleRequests);
+var dispatcher = require('httpdispatcher');
+
+
+    server.listen(PORT, HOST);
+
+    //Handler
+    function handleRequests(request,response){
+        try{
+            console.log(request.url);
+            
+            dispatcher.dispatch(request, response);
+        }catch(err){
+            console.log(err);
+        }
+    }
+
+    //where static files are stored
+    dispatcher.setStatic("../Database/resources");
+    
+    //login screen
+    dispatcher.onGet("/login", function(req,res){
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        res.end('Login Page');
+    });
+    
+    //profile screen
+    dispatcher.onGet("/profile", function(req,res){
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        res.end('Profile Page');
+    });
