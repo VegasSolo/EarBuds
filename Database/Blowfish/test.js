b7484495bc1e604377b269a61c925890a2c32698
Dillon VanBuskirk <dillonuark@gmail.com>
2016-03-12 22:23:05 +0000

    mod key

--- Database/Blowfish/test.js
+++ Database/Blowfish/test.js
@@ -1,8 +1,8 @@
 
 var Encryption = require('./encryption.js');
 var e = new Encryption();
-var key = "test_key";
-var text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris fringilla, risus id varius porttitor, metus ligula consectetur mi, id vulputate sapien tortor sit amet nunc.";
+var key = "not_e4rbud5/!key";
+var text = "passwordtest";
 
 var encrypted = e.encrypt(text, key);
 console.log("Encrypted text: "+encrypted);
