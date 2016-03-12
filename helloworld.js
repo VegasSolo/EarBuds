f7a9ec9507a2b167414cb394a5dfa8fded4d0a76
Dillon VanBuskirk <dillonuark@gmail.com>
2016-03-12 22:09:53 +0000

    testing crypt

--- helloworld.js
+++ helloworld.js
@@ -5,3 +5,11 @@ http.createServer(function (req,res) {
     res.end('Hello World\n');
 }).listen(process.env.PORT, process.env.IP);
 console.log("Hello, World");
+
+var bcrypt = require('bcrypt');
+bcrypt.genSalt(10, function(err, salt) {
+    bcrypt.hash('B4c0/\/', salt, function(err, hash) {
+        // Store hash in your password DB.
+        console.log(hash);
+    });
+});
