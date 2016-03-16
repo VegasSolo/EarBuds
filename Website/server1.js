4ab24bf3524a01e2ea98e2658bce8642c22b5584
ezimkin <nrmize@email.uark.edu>
2016-03-16 01:21:15 +0000

    updated server1.js and added a profile.html page

--- Website/server1.js
+++ Website/server1.js
@@ -4,8 +4,8 @@ var fs = require('fs');
 var url = require('url');
 var express = require('express');
 var bodyParser = require('body-parser');
-var session = require('client-sessions');
-var cookieParser = require('cookie-parser');
+var session = require('express-session');
+var RedisStore = require('connect-redis')(session);
 
 /* Vars */
 var PORT = process.env.PORT;
@@ -15,18 +15,16 @@ var app = express();
 // Create application/x-www-form-urlencoded parser
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
-//Get location of static webpage files
+//Get location of static webpage files ~/public/...
 app.use(express.static('public'));
-
-//Initialize cookieParser
-app.use(cookieParser());
 
 //Initialize sessions
 app.use(session({
-    cookieName: 'session', //name of cookie
     secret: '9t8YCmDaQb9NcznMC0F1', //string key for encryption
-    duration: 2 * 60 * 60 * 1000, //how long the session is active in ms
-    activeDuration: 10 * 60 * 1000 //how long the duration can be extended after use
+    maxAge: 60*60*1000,  //expires in an hour
+    resave: false,
+    saveUninitialized: true,
+    cookie: {secure: true}
 }))
 
 /*  
@@ -36,36 +34,88 @@ app.use(session({
 **  OR
 **  app.post(</page>, urlencodedParser, function(req,res)){}  **FOR POST
 **  -------------------------------------------------------
+**  Check if a session exists:
+**  if(req.session.user){}
+**
+**  Destroy a session:
+**  req.session.destroy(function(err){
+**      if(err){
+**          console.log(err);
+**      }
+**  });
+**  -------------------------------------------------------
 */  
 
-//Main page GET
+/* 
+**  -------------------------------------------------------
+**  Routing Pages
+**  -------------------------------------------------------
+*/ 
+
+//Login page
 app.get('/', function(req,res){
-    res.sendFile(__dirname+'/'+'login.html');
+    if(req.session.user){
+        res.sendFile(__dirname+'/'+'profile.html');
+    }
+    else{
+        res.sendFile(__dirname+'/'+'login.html');
+    }
 })
-
-//Main page POST
 app.post('/', urlencodedParser, function(req,res){
-    res.sendFile(__dirname+'/'+'login.html');
+    if(req.session.user){
+        res.sendFile(__dirname+'/'+'profile.html');
+    }
+    else{
+        res.sendFile(__dirname+'/'+'login.html');
+    }
 })
 
-//Process login POST
+//User Page
+app.get('/profile', function(req,res){
+    res.sendFile(__dirname+'/'+'profile.html');
+})
+app.post('/profile', urlencodedParser, function(req,res){
+    res.sendFile(__dirname+'/'+'profile.html');
+})
+
+/* 
+**  -------------------------------------------------------
+**  Process Pages
+**  -------------------------------------------------------
+*/ 
+
+//Process login
 app.post('/POST_login', urlencodedParser, function(req,res){
-    //set user to a session var
-    if(req.session && req.session.user){
-        req.session.user = req.body.user;
+    //check if in a session
+    if(req.session.user && req.session){
+     
+        res.sendFile(__dirname+'/'+'profile.html');  
     }
     else{
         req.session.user = req.body.user;
         req.session.fname = req.body.fname;
         req.session.lname = req.body.lname;
         req.session.email = req.body.email;
+        res.sendFile(__dirname+'/'+'profile.html');
     }
-        
-    
-    res.end('Succesfully recieved login request for User: '+req.session.user);
 })
 
-//server start
+//Process logout
+app.post('/POST_logout', urlencodedParser, function(req,res){
+    req.session.destroy(function(err){
+        if(err){
+            console.log(err);
+        }
+    });
+    res.sendFile(__dirname+'/'+'login.html');
+})
+
+/* 
+**  -------------------------------------------------------
+**  Server Start
+**  -------------------------------------------------------
+*/ 
+
 var server = app.listen(PORT, function(){
     console.log('Server listening at http://%s:%s', IP, PORT);
 })
