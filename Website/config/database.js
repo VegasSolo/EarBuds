835c08b520be0893074933f53e321f97a3ddb6a9
ezimkin <nrmize@email.uark.edu>
2016-03-17 22:01:38 +0000

    update database to be local

--- Website/config/database.js
+++ Website/config/database.js
@@ -1,6 +1,6 @@
 // config/database.js
 module.exports = {
 
-    'url' : 'mongodb://admin:earbud@jello.modulusmongo.net:27017/xUt8ipam' // Dillon's MongoDB
+    'url' : process.env.IP // Dillon's MongoDB
 
 };
