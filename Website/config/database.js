3718048329e145466861a9df9afbfd53f0ef13c3
Dillon VanBuskirk <dillonuark@gmail.com>
2016-03-30 18:00:56 +0000

    a

--- Website/config/database.js
+++ Website/config/database.js
@@ -1,6 +1,6 @@
 // config/database.js
 module.exports = {
 
-    'url' : 'mongodb://'+process.env.IP // Dillon's MongoDB
+    'url' : 'mongodb://<admin>:<earbud>@jello.modulusmongo.net:27017/xUt8ipam' // Dillon's MongoDB
 
 };
