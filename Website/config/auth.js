bbae7a53691d34cb3c35305d8256e8c3e2de272e
osvvald <obobadil@email.uark.edu>
2016-04-20 02:45:24 +0000

    hi ozvaldo

--- Website/config/auth.js
+++ Website/config/auth.js
@@ -6,7 +6,7 @@ module.exports = {
     'spotifyAuth' : {
         'clientID'      : 'bf0bab1fc5bd47309d593a087928d05b', // your App ID
         'clientSecret'  : '1590548fd1ca4f2f9be23d102ab22aa1', // your App Secret
-        'callbackURL'   : 'https://'+'seproject-ezimkin.c9users.io'+'/auth/spotify/callback' // CHANGE TO YOUR OWN URL
+        'callbackURL'   : 'https://'+'seproject-osvvald.c9users.io'+'/auth/spotify/callback' // CHANGE TO YOUR OWN URL
     },
 
 };
